43c8c936988d161a749daff807924c983eb57b41
mahirovargas <jadiel3vargas@gmail.com>
2025-08-24 19:16:37 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Map deo',
+  webDir: 'www'
+};
+
+export default config;
